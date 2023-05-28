a40180d8fd71c577258cfa5a3fbffe1ff7982498
andy <ztao8607@gmail.com>
2023-05-28 18:59:38 +0800

    modify bootstrap layout

--- result/html.py
+++ result/html.py
@@ -7,13 +7,48 @@ def outputWithHtml():
 
     allRecords = getAllEvaluations()
     # Create the HTML table
-    htmlTable = "<table class='table table-striped'>\n<thead><tr><th>ID</th><th>Name</th><th style='max-width: 50%'>Prompt</th><th style='max-width: 20%'>Response</th><th>Operations</th></tr></thead>\n<tbody>"
+    htmlTable = """
+    <table class="table table-bordered table-hover align-middle">
+        <thead>
+            <tr>
+                <th  class="w-10">#</th>
+                <th  class="w-10">Name</th>
+                <th  class="w-30">Response</th>
+                <th  class="w-10">Operations</th>
+                <th  class="w-40">Prompt</th>
+            </tr>
+        </thead>
+        <tbody>
+    """
     for record in allRecords:
         print(record['id'], record['status'])
         if record['status'] == 'finish':
-            htmlTable += f"<tr><td>{record['id']}</td><td>{record['name']}</td><td style='max-width: 50%'>{record['prompt']}</td><td style='max-width: 20%'>{record['evaluation']}</td><td><button class='btn btn-primary'>get result</button></td></tr>\n"
+            htmlTable += f"""
+            <tr>
+                <th class="w-10">{record['id']}</th>
+                <td class="w-10">{record['name']}</td>
+                <td class="w-30" style="overflow: hidden; text-overflow:ellipsis; white-space: nowrap;">{record['evaluation']}</td>
+                <td class="w-10">
+                    <button class='btn btn-primary'>get result</button>
+                </td>
+                <td class="w-40" style="overflow: hidden; text-overflow:ellipsis; white-space: nowrap;">{record['prompt']}</td>
+                
+            </tr>
+            """
         else:
-            htmlTable += f"<tr><td>{record['id']}</td><td>{record['name']}</td><td style='max-width: 50%'>{record['prompt']}</td><td style='max-width: 20%'>{record['evaluation']}</td><td>{record['status']}</td></tr>\n"
-    htmlTable += "</tbody></table>"
+            htmlTable += f"""
+            <tr>
+                <th class="w-10">{record['id']}</th>
+                <td class="w-10">{record['name']}</td>
+                <td class="w-30" style="overflow: hidden; text-overflow:ellipsis; white-space: nowrap;">{record['evaluation']}</td>
+                <td class="w-10">{record['status']}</td>
+                <td class="w-40" style="overflow: hidden; text-overflow:ellipsis; white-space: nowrap;">{record['prompt']}</td>
+                
+            </tr>
+            """
+    htmlTable += """
+        </tbody>
+    </table>
+    """
     
     return htmlTable
