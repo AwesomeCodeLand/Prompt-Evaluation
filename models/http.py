46d468013ce941f6e15642d7f2253a5c6ece5348
andy <ztao8607@gmail.com>
2023-05-29 22:59:55 +0800

    Supply toJson function in Eva class

--- models/http.py
+++ models/http.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 from typing import List, Dict, Optional
 import json
+
 # class GptRequest(BaseModel):
-    # {
+# {
 #     "eval": {
 #         "model": "gpt-3.5-turbo",
 #         "messages": [
@@ -20,15 +21,16 @@ import json
 #         "answer": ""
 #     }
 # }
-    
-    
+
 
 class Message(BaseModel):
     role: str
     content: str
+
     def toJSON(self):
-        return json.dumps(self, default=lambda o: o.__dict__, 
-            sort_keys=True, indent=4)
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
 class Eval(BaseModel):
     model: str
     messages: List[Message]
@@ -40,8 +42,16 @@ class Eval(BaseModel):
     stop: Optional[List[str]] = None
     top_n: Optional[float] = None
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+
 class Stand(BaseModel):
     answer: str
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
 
 class GptRequest(BaseModel):
     eval: Eval
