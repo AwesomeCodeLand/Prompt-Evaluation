5a48a807eb8ec64db1610482ef78e5b519972936
andy <ztao8607@gmail.com>
2023-06-06 13:54:15 +0800

    Set response as optional

--- models/http.py
+++ models/http.py
@@ -36,7 +36,7 @@ class Stand(BaseModel):
 class GptRequest(BaseModel):
     eval: Eval
     stand: Stand
-    response: str
+    response: Optional[str] = None
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
